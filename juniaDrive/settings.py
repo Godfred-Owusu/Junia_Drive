1497650abb28bf4bc13a3e8f92982787a241d246
Godfred Mireku Owusu <103333221+Godfred-Owusu@users.noreply.github.com>
2024-12-03 02:02:24 +0100

    Update settings.py

--- juniaDrive/settings.py
+++ juniaDrive/settings.py
@@ -31,10 +31,10 @@ SECRET_KEY = 'django-insecure-zk^=)c+_^3x6@vl!)(qvxs+d75c#-lb=mu#o$q)#fu8t8-zrf8
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-DEBUG = False
+DEBUG = "False"
 
-# ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ["vercel.app",".now.sh", "127.0.0.1", "localhost" ]
+ ALLOWED_HOSTS = ["*"]
+#ALLOWED_HOSTS = ["vercel.app",".now.sh", "127.0.0.1", "localhost" ]
 # ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
