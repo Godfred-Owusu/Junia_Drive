655c41c130c9368219246d87ab6c714f84b9d59a
godfr <gowusu018@gmail.com>
2024-12-03 02:29:55 +0100

    add allowed url

--- juniaDrive/settings.py
+++ juniaDrive/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-zk^=)c+_^3x6@vl!)(qvxs+d75c#-lb=mu#o$q)#fu8t8-zrf8
 DEBUG = "False"
 
 ALLOWED_HOSTS = ["*"]
-#ALLOWED_HOSTS = ["vercel.app",".now.sh", "127.0.0.1", "localhost" ]
+# ALLOWED_HOSTS = ["vercel.app",".now.sh", "127.0.0.1", "localhost" ]
 # ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
@@ -170,7 +170,10 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+# here
+CSRF_TRUSTED_ORIGINS = [
+    "https://junia-drive.onrender.com"
+]
 
 # DATABASES = {
 #     'default': {
